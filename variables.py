d15afa67d29014218d501ff24e0765fbb3438eff
K Bhanu <bhanukagitha30@gmail.com>
2026-08-04 23:03:16 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+name="bhanu"
+age=18
+collage="SRKR"
+branch="CSIT"
+print(name,age,collage,branch,sep="\n")
+age=20
+print(name,age,collage,branch,sep="\n")
+bike,car,cycle="pulsar","scorpio","hero"
+print(bike,car,cycle,sep="\n")
+x=y=z=10
+print(x,y,z,sep="\n")
+print(type(name),type(age),type(collage),type(branch),sep="\n")
+# 1name="bhanu"  # invalid variable name number cannot be used at the start of the variable name
+# name@="bhanu"  # invalid variable name special characters cannot be used in the variable name
+# reg no="718"  # invalid variable name space cannot be used in the variable name 
+print("variable is a name given to a memory location that stores data.")
+print("variables are used to store data that can be used and manipulated in a program.")
+print("identifiers are noting but the name given to a variable or function or class.")
+print("key words are the reserved words that have special meaning in python and cannot be used as identifiers.example : if ,else etc.")
+print("dynamic typing means that the type of a variable is determined at runtime and can change during the execution of the program.")
+print("type() function is used to check the type of a variable.")
+print("id() function is used to check the memory address of a variable.")
+print("del statement is used to delete a variable from the memory.")
+print("yes two variables can have the same value but they cannot have the same name.example : name1 and name2 can have the same value but they cannot have the same name.")
