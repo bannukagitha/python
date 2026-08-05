9227d3490b3631203ae4c747920241f59cb17fa8
K Bhanu <bhanukagitha30@gmail.com>
2026-08-05 19:56:29 +0530

    data types

---
+++ data.py
@@ -0,0 +1,40 @@
+age = 20
+marks = 95
+temperature = -8
+population = 1500000
+zero = 0
+print(age,marks,temperature,population,zero,sep="\n")
+print(type(age),type(marks),type(temperature),type(population),type(zero),sep="\n")
+age2=20
+print(type(age2),type(age),sep="\n")
+print("data is any information that can be stored and manipulated in a program .")
+print("data types are the classification of data that tells the compiler or interpreter how the programmer intends to use the data.")
+print("we need to use data types because they help the compiler or interpreter to understand how to handle the data and what operations can be performed on it.")
+print("interger data type is used to store whole numbers without decimal points.")
+print(0,"is a integer data type because it is a whole number without decimal points.")
+print("20","is a string data type .")
+print("type() function is used to check the data type of a variable.")
+print(type(25))
+#integer
+print(type("25"))
+#string
+print(type(-7))
+#integer
+a = 10.5
+b = 2.5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+cgpa = 8.75
+height = 5.9
+weight = 72.4
+price = 99.99
+temperature = -12.5
+print(cgpa,height,weight,price,temperature,sep="\n")
+print(type(cgpa),type(height),type(weight),type(price),type(temperature),sep="\n") 
+print(type(100.0))
+print(type(100))
+print(type(-5.75))
