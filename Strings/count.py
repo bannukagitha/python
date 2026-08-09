9fd5ec04c53fca5efe94b7e83165a2cb2f4ea69b
K Bhanu <bhanukagitha30@gmail.com>
2026-08-09 12:55:24 +0530

    allcount

--- Strings/count.py
+++ Strings/count.py
@@ -1,9 +1,15 @@
 text=input()
 vowels=0
 consonants=0
+digits=0
+special=0
 for ch in text:
   if ch in "aeiouAEIOU":
     vowels+=1
-  else:
+  elif ch.isalpha():
     consonants+=1
-print(vowels,consonants)
+  elif ch.isdigit():
+    digits+=1
+  elif not ch.isalnum() and not ch.isspace():
+    special+=1
+print(vowels,consonants,digits,special)
