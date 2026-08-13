d2d31aa6fc62c1bc6c781e3cc61b9301a69f6a19
K Bhanu <bhanukagitha30@gmail.com>
2026-08-13 22:43:48 +0530

    slidingwindow

---
+++ list/slidingwindow.py
@@ -0,0 +1,9 @@
+n,window_size=map(int,input().split())
+arr=list(map(int,input().split()))
+window_sum=sum(arr[:window_size])
+max_window=window_size
+for i in range(window_size,n):
+  window_sum+=arr[i]
+  window_sum-=arr[i-window_size]
+  max_window=max(max_window,window_sum)
+print(max_window)
