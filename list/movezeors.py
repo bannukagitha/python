998bf903899cb46236d96c47904648469eb89347
K Bhanu <bhanukagitha30@gmail.com>
2026-08-17 22:51:00 +0530

    movezeros

---
+++ list/movezeors.py
@@ -0,0 +1,11 @@
+n=len(nums)
+        left=0
+        right=0
+        while right<n:
+            if nums[left]==0 and nums[right]!=0:
+                nums[left],nums[right]=nums[right],nums[left]
+            if nums[left]!=0:
+                left+=1
+            right+=1
+            
+        return nums
