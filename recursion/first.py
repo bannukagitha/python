7097663afa41da346d476eb7b66f9a20ac762d81
K Bhanu <bhanukagitha30@gmail.com>
2026-08-19 11:42:31 +0530

    resusion

--- recursion/first.py
+++ recursion/first.py
@@ -3,7 +3,16 @@ def print_name(i,n):
     return 
   print("Bhanu")
   print_name(i+1,n)
-
-n=int(input())
-i=1
-print_name(i,n)
+def linear(j,n):
+  if j>n:
+    return
+  print(j)
+  linear(j+1,n)
+def main():
+  n=int(input())
+  i=1
+  j=1
+  print_name(i,n)
+  linear(j,n)
+if __name__=="__main__":
+  main()
