7042b5c843a7376488a058c62df9d67cd4f135e8
K Bhanu <bhanukagitha30@gmail.com>
2026-08-20 09:52:05 +0530

    back

--- recursion/first.py
+++ recursion/first.py
@@ -9,11 +9,19 @@ def linear(j,n):
   print(j)
   linear(j+1,n)
 
+def back(k,n):
+  if k<1:
+    return 
+  print(k)
+  back(k-1,n)
+
 def main():
   n=int(input())
   i=1
   j=1
+  k=n
   print_name(i,n)
   linear(j,n)
+  back(k,n)
 if __name__=="__main__":
   main()
