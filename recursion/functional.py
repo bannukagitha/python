ee6894ac77d9a0b420d3e62252e5fc97a1affa97
K Bhanu <bhanukagitha30@gmail.com>
2026-08-20 10:24:23 +0530

    fact

--- recursion/functional.py
+++ recursion/functional.py
@@ -2,10 +2,14 @@ def totalsum(i):
   if i==1:
     return 1
   return i+totalsum(i-1)
-  
+def fact(i):
+  if i==1:
+    return 1
+  return i*fact(i-1)
 def main():
   n=int(input())
   print(totalsum(n))
+  print(fact(n))
   
 if __name__=="__main__":
   main()
